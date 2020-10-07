8b7958fe695f6f7eb3cb23bb74d2fc43238da680
tamuatsu <a-tamura@spirits-gr.com>
2020-10-07 11:45:24 +0900

    adding port

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log(`app is running on port ${process.env.PORT}`);
 })
